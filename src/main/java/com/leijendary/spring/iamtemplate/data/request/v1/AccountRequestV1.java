b56bf5c94e17e7fed6afe498fd943577f95690f7
Jonathan Leijendekker <jonathan.leijendekker@novare.com.hk>
2021-07-29 17:20:14 +0800

    changed account type validation to not null

--- src/main/java/com/leijendary/spring/iamtemplate/data/request/v1/AccountRequestV1.java
+++ src/main/java/com/leijendary/spring/iamtemplate/data/request/v1/AccountRequestV1.java
@@ -3,12 +3,12 @@ package com.leijendary.spring.iamtemplate.data.request.v1;
 import com.leijendary.spring.iamtemplate.validator.annotation.v1.AccountTypeV1;
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class AccountRequestV1 {
 
-    @NotEmpty(message = "validation.required")
+    @NotNull(message = "validation.required")
     @AccountTypeV1
     private String type;
 }
