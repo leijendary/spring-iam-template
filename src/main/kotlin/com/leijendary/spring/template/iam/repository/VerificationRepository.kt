d8039133e7915bbb30fe2f33ab600e520ab15e2a
Jonathan Leijendekker <8501171+leijendary@users.noreply.github.com>
2023-02-21 19:04:47 +0100

    WIP: VerificationRepository.kt

--- src/main/kotlin/com/leijendary/spring/template/iam/repository/VerificationRepository.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/repository/VerificationRepository.kt
@@ -4,8 +4,30 @@ import com.leijendary.spring.template.iam.entity.Verification
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
+private val codeSource = listOf("data", "Verification", "code")
+
 interface VerificationRepository : JpaRepository<Verification, Long> {
     fun deleteAllByUserIdAndType(userId: UUID, type: String)
 
     fun findFirstByCodeAndType(code: String, type: String): Verification?
+
+    fun findFirstByCodeAndTypeAndUserId(
+        code: String,
+        type: String,
+        userId: UUID
+    ): Verification?
+
+    fun findFirstByCodeAndTypeOrThrow(code: String, type: String): Verification {
+        return findFirstByCodeAndType(code, type) 
+            ?: throw ResourceNotFoundException(codeSource, code)
+    }
+
+    fun findFirstByCodeAndTypeAndUserIdOrThrow(
+        code: String,
+        type: String,
+        userId: UUID
+    ): Verification {
+        return findFirstByCodeAndTypeAndUserId(code, type, userId) 
+            ?: throw ResourceNotFoundException(codeSource, code)
+    }
 }
