5658aebe8ec9f62886cd657e50761d0c441b0a2d
leijendary <jonathanleijendekker@gmail.com>
2023-02-21 19:47:32 +0100

    updated verification to support with user ID query

--- src/main/kotlin/com/leijendary/spring/template/iam/repository/VerificationRepository.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/repository/VerificationRepository.kt
@@ -1,33 +1,26 @@
 package com.leijendary.spring.template.iam.repository
 
+import com.leijendary.spring.template.iam.core.exception.ResourceNotFoundException
 import com.leijendary.spring.template.iam.entity.Verification
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-private val codeSource = listOf("data", "Verification", "code")
+private val sourceCode = listOf("data", "Verification", "code")
 
 interface VerificationRepository : JpaRepository<Verification, Long> {
     fun deleteAllByUserIdAndType(userId: UUID, type: String)
 
     fun findFirstByCodeAndType(code: String, type: String): Verification?
 
-    fun findFirstByCodeAndTypeAndUserId(
-        code: String,
-        type: String,
-        userId: UUID
-    ): Verification?
+    fun findFirstByCodeAndTypeAndUserId(code: String, type: String, userId: UUID): Verification?
 
     fun findFirstByCodeAndTypeOrThrow(code: String, type: String): Verification {
-        return findFirstByCodeAndType(code, type) 
-            ?: throw ResourceNotFoundException(codeSource, code)
+        return findFirstByCodeAndType(code, type)
+            ?: throw ResourceNotFoundException(sourceCode, code)
     }
 
-    fun findFirstByCodeAndTypeAndUserIdOrThrow(
-        code: String,
-        type: String,
-        userId: UUID
-    ): Verification {
-        return findFirstByCodeAndTypeAndUserId(code, type, userId) 
-            ?: throw ResourceNotFoundException(codeSource, code)
+    fun findFirstByCodeAndTypeAndUserIdOrThrow(code: String, type: String, userId: UUID): Verification {
+        return findFirstByCodeAndTypeAndUserId(code, type, userId)
+            ?: throw ResourceNotFoundException(sourceCode, code)
     }
 }
--- src/main/kotlin/com/leijendary/spring/template/iam/validator/VerificationValidator.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/validator/VerificationValidator.kt
@@ -1,27 +1,28 @@
 package com.leijendary.spring.template.iam.validator
 
-import com.leijendary.spring.template.iam.core.exception.ResourceNotFoundException
 import com.leijendary.spring.template.iam.core.exception.StatusException
 import com.leijendary.spring.template.iam.core.extension.transactional
 import com.leijendary.spring.template.iam.core.util.RequestContext.now
-import com.leijendary.spring.template.iam.entity.UserCredential
+import com.leijendary.spring.template.iam.core.util.RequestContext.userIdOrNull
 import com.leijendary.spring.template.iam.entity.Verification
 import com.leijendary.spring.template.iam.repository.VerificationRepository
 import org.springframework.http.HttpStatus.BAD_REQUEST
 import org.springframework.http.HttpStatus.GONE
 import org.springframework.stereotype.Component
+import java.util.*
 
 @Component
 class VerificationValidator(private val verificationRepository: VerificationRepository) {
-    companion object {
-        private val SOURCE = listOf("data", "Verification", "code")
-    }
-
     fun validate(code: String, type: String, deviceId: String): Verification {
+        val userId = userIdOrNull?.let { UUID.fromString(it) }
         val verification = transactional(readOnly = true) {
-            verificationRepository
-                .findFirstByCodeAndType(code, type)
-                ?: throw ResourceNotFoundException(SOURCE, code)
+            if (userId != null) {
+                verificationRepository
+                    .findFirstByCodeAndTypeAndUserIdOrThrow(code, type, userId)
+            } else {
+                verificationRepository
+                    .findFirstByCodeAndTypeOrThrow(code, type)
+            }
         }!!
         val expiresAt = verification.expiresAt!!
 
@@ -35,10 +36,8 @@ class VerificationValidator(private val verificationRepository: VerificationRepo
             throw StatusException(source, "validation.verification.expired", GONE)
         }
 
-        val field = verification.field
-
-        // If the validation is for a phone, the deviceId should match.
-        if (field == UserCredential.Type.PHONE.value && deviceId != verification.deviceId) {
+        // Device ID should match
+        if (deviceId != verification.deviceId) {
             val source = listOf("data", "Verification", "deviceId")
 
             throw StatusException(source, "validation.deviceId.notMatch", BAD_REQUEST)
