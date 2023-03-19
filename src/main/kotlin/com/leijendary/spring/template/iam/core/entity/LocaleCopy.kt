02a0805aa1bc7f12001efa83238582e7cede93bb
leijendary <jonathanleijendekker@gmail.com>
2023-03-19 20:35:03 +0100

    removed unused classes

--- src/main/kotlin/com/leijendary/spring/template/iam/core/entity/LocaleCopy.kt
+++
@@ -1,9 +0,0 @@
-package com.leijendary.spring.template.iam.core.entity
-
-import jakarta.persistence.MappedSuperclass
-
-@MappedSuperclass
-open class LocaleCopy : AppEntity() {
-    var language: String? = null
-    var ordinal: Int = 0
-}
