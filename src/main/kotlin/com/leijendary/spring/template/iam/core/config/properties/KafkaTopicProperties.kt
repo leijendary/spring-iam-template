f14d330350f1b0084ec02b8e02cd93136459e44b
leijendary <jonathanleijendekker@gmail.com>
2023-03-19 17:50:49 +0100

    removed getters from KafkaTopicProperties

--- src/main/kotlin/com/leijendary/spring/template/iam/core/config/properties/KafkaTopicProperties.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/core/config/properties/KafkaTopicProperties.kt
@@ -3,19 +3,4 @@ package com.leijendary.spring.template.iam.core.config.properties
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("spring.kafka.topic")
-class KafkaTopicProperties : HashMap<String, String>() {
-    companion object {
-        const val TOPIC_SAMPLE_CREATE = "sample.create"
-        const val TOPIC_SAMPLE_UPDATE = "sample.update"
-        const val TOPIC_SAMPLE_DELETE = "sample.delete"
-    }
-
-    val sampleCreate: String
-        get() = get(TOPIC_SAMPLE_CREATE)!!
-
-    val sampleUpdate: String
-        get() = get(TOPIC_SAMPLE_UPDATE)!!
-
-    val sampleDelete: String
-        get() = get(TOPIC_SAMPLE_DELETE)!!
-}
+class KafkaTopicProperties : HashMap<String, String>()
