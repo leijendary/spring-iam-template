9aac6001c1d953081a8915dc5a5233eef28d0d98
leijendary <jonathanleijendekker@gmail.com>
2023-04-16 03:02:07 +0200

    renamed fields to lowerCamelCase

--- src/main/kotlin/com/leijendary/spring/template/iam/entity/listener/VerificationListener.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/entity/listener/VerificationListener.kt
@@ -1,12 +1,12 @@
 package com.leijendary.spring.template.iam.entity.listener
 
-import com.leijendary.spring.template.iam.core.util.BeanContainer.VERIFICATION_EVENT
+import com.leijendary.spring.template.iam.core.util.BeanContainer.verificationEvent
 import com.leijendary.spring.template.iam.entity.Verification
 import jakarta.persistence.PostPersist
 
 class VerificationListener {
     @PostPersist
     fun onCreate(verification: Verification) {
-        VERIFICATION_EVENT.notify(verification)
+        verificationEvent.notify(verification)
     }
 }
