13806e7b920566a1393dd1f4e1eaa2cb3e66bfa3
leijendary <jonathanleijendekker@gmail.com>
2023-04-27 17:28:06 +0200

    Updated push message image field name

--- src/main/kotlin/com/leijendary/spring/template/iam/model/PushMessage.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/model/PushMessage.kt
@@ -2,4 +2,4 @@ package com.leijendary.spring.template.iam.model
 
 import java.util.*
 
-data class PushMessage(val userId: UUID, val title: String, val body: String, val imageUrl: String? = null)
+data class PushMessage(val userId: UUID, val title: String, val body: String, val image: String? = null)
