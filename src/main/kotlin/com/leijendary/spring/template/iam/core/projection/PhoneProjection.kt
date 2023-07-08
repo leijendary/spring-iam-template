2aaddb4da09c4fc27df827467ae94acce646d387
leijendary <jonathanleijendekker@gmail.com>
2023-07-08 23:00:57 +0200

    added JsonIgnore for fullPhone

--- src/main/kotlin/com/leijendary/spring/template/iam/core/projection/PhoneProjection.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/core/projection/PhoneProjection.kt
@@ -1,9 +1,12 @@
 package com.leijendary.spring.template.iam.core.projection
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+
 interface PhoneProjection {
     var countryCode: String?
     var phone: String?
 
+    @get:JsonIgnore
     val fullPhone: String
         get() = "$countryCode $phone"
 }
