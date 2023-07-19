656b533e5497828643b8ff2fd2e68699ac3f4ae3
leijendary <jonathanleijendekker@gmail.com>
2023-07-19 22:54:35 +0200

    Moved VerificationEvent out of BeanContainer

--- src/main/kotlin/com/leijendary/spring/template/iam/core/util/BeanContainer.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/core/util/BeanContainer.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.leijendary.spring.template.iam.core.config.properties.AuthProperties
 import com.leijendary.spring.template.iam.core.storage.S3Storage
 import com.leijendary.spring.template.iam.core.util.SpringContext.Companion.getBean
-import com.leijendary.spring.template.iam.event.VerificationEvent
 import io.micrometer.tracing.Tracer
 import org.springframework.transaction.PlatformTransactionManager
 
@@ -14,5 +13,4 @@ object BeanContainer {
     val s3Storage by lazy { getBean(S3Storage::class) }
     val tracer by lazy { getBean(Tracer::class) }
     val transactionManager by lazy { getBean(PlatformTransactionManager::class) }
-    val verificationEvent by lazy { getBean(VerificationEvent::class) }
 }
--- src/main/kotlin/com/leijendary/spring/template/iam/entity/listener/VerificationListener.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/entity/listener/VerificationListener.kt
@@ -1,10 +1,13 @@
 package com.leijendary.spring.template.iam.entity.listener
 
-import com.leijendary.spring.template.iam.core.util.BeanContainer.verificationEvent
+import com.leijendary.spring.template.iam.core.util.SpringContext.Companion.getBean
 import com.leijendary.spring.template.iam.entity.Verification
+import com.leijendary.spring.template.iam.event.VerificationEvent
 import jakarta.persistence.PostPersist
 
 class VerificationListener {
+    private val verificationEvent by lazy { getBean(VerificationEvent::class) }
+
     @PostPersist
     fun onCreate(verification: Verification) {
         verificationEvent.notify(verification)
