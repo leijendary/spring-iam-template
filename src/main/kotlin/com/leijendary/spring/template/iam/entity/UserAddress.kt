335350597ca776eea8cac880e7e3659d337ba0e6
leijendary <jonathanleijendekker@gmail.com>
2023-07-28 12:07:40 +0200

    Removed data type for primary

--- src/main/kotlin/com/leijendary/spring/template/iam/entity/UserAddress.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/entity/UserAddress.kt
@@ -30,7 +30,7 @@ class UserAddress : UUIDProjection {
     lateinit var countryCode: String
     var longitude: Double = 0.0
     var latitude: Double = 0.0
-    var primary: Boolean = false
+    var primary = false
 
     @CreatedDate
     @Column(updatable = false)
