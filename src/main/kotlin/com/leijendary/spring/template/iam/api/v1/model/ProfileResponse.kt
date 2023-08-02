4fbfc1d1b7b50eb26535d6b21e91f7b3e45b4200
leijendary <jonathanleijendekker@gmail.com>
2023-08-02 14:21:17 +0200

    Made firstName, lastName, and email response required

--- src/main/kotlin/com/leijendary/spring/template/iam/api/v1/model/ProfileResponse.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/api/v1/model/ProfileResponse.kt
@@ -3,10 +3,10 @@ package com.leijendary.spring.template.iam.api.v1.model
 import java.io.Serializable
 
 data class ProfileResponse(
-    val firstName: String?,
+    val firstName: String,
     val middleName: String?,
-    val lastName: String?,
-    val email: String?,
+    val lastName: String,
+    val email: String,
     val emailVerified: Boolean = false,
     val countryCode: String?,
     val phone: String?,
