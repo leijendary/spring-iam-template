c3dac1aa2b7d416893caac67e4afa8527786945f
leijendary <jonathanleijendekker@gmail.com>
2023-08-03 22:45:44 +0200

    Made NotificationProducer @Retryable

--- src/main/kotlin/com/leijendary/spring/template/iam/message/NotificationProducer.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/message/NotificationProducer.kt
@@ -9,6 +9,7 @@ import com.leijendary.spring.template.iam.model.EmailMessage
 import com.leijendary.spring.template.iam.model.PushMessage
 import com.leijendary.spring.template.iam.model.SmsMessage
 import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Component
 
 @Component
@@ -16,18 +17,21 @@ class NotificationProducer(
     private val kafkaTemplate: KafkaTemplate<String, String>,
     private val kafkaTopicProperties: KafkaTopicProperties
 ) {
+    @Retryable
     fun email(emailMessage: EmailMessage) {
         val topic = kafkaTopicProperties.nameOf(NOTIFICATION_EMAIL)
 
         kafkaTemplate.send(topic, emailMessage.toJson())
     }
 
+    @Retryable
     fun push(pushMessage: PushMessage) {
         val topic = kafkaTopicProperties.nameOf(NOTIFICATION_PUSH)
 
         kafkaTemplate.send(topic, pushMessage.toJson())
     }
 
+    @Retryable
     fun sms(smsMessage: SmsMessage) {
         val topic = kafkaTopicProperties.nameOf(NOTIFICATION_SMS)
 
