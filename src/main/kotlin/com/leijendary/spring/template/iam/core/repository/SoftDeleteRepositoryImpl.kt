cf906b951bbd4a8cb6eb23a917c6aec9935f9691
leijendary <jonathanleijendekker@gmail.com>
2023-10-18 13:54:04 +0200

    removed transactional for SoftDeleteRepositoryImpl

--- src/main/kotlin/com/leijendary/spring/template/iam/core/repository/SoftDeleteRepositoryImpl.kt
+++ src/main/kotlin/com/leijendary/spring/template/iam/core/repository/SoftDeleteRepositoryImpl.kt
@@ -2,22 +2,17 @@ package com.leijendary.spring.template.iam.core.repository
 
 import com.leijendary.spring.template.iam.core.entity.SoftDeleteEntity
 import jakarta.persistence.EntityManager
-import jakarta.persistence.PersistenceContext
 import org.springframework.data.auditing.DateTimeProvider
 import org.springframework.data.domain.AuditorAware
 import org.springframework.stereotype.Repository
-import org.springframework.transaction.annotation.Transactional
 import java.time.OffsetDateTime
 
 @Repository
 class SoftDeleteRepositoryImpl<T : SoftDeleteEntity>(
     private val auditorAware: AuditorAware<String>,
     private val dateTimeProvider: DateTimeProvider,
-
-    @PersistenceContext
     private val entityManager: EntityManager
 ) : SoftDeleteRepository<T> {
-    @Transactional
     override fun softDelete(entity: T) {
         val merged = entityManager.merge(entity)
         merged.apply {
